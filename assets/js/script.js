f5b545efb6548ec65011ac256c56713fedd801a2
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-05-23 17:56:16 +0600

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,37 +59,9 @@ function openNav() {
 
 
 
-//  Tab js here
-    // function openTab(evt, tabName) {
-    //     var i, tabcontent, tablinks;
-    //     tabcontent = document.getElementsByClassName("tabcontent");
-    //     for (i = 0; i < tabcontent.length; i++) {
-    //         tabcontent[i].style.display = "none";
-    //     }
-    //     tablinks = document.getElementsByClassName("tablinks");
-    //     for (i = 0; i < tablinks.length; i++) {
-    //         tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //     }
-    //     document.getElementById(tabName).style.display = "block";
-    //     evt.currentTarget.className += " active";
-    // }
-    // function buttonNext(event, nextName) {
-    //     tabcontent = document.getElementsByClassName("tabcontent");
-    //     tablinks = document.getElementsByClassName("tablinks");
-    //     var divName = event.currentTarget.parentNode.parentNode.getElementsByTagName('div');
-    //     for (var i = 0; i < tablinks.length; i++) {
-    //         if (nextName == divName[i].id) {
-    //             tablinks[i].className = "tablinks active";
-    //             tabcontent[i].style.display = "block";
-    //         }
-    //         else {
-    //             tablinks[i].className = "tablinks";
-    //             tabcontent[i].style.display = "none";
-    //         }
-    //     }
-    // }
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
+
+    // // Get the element with id="defaultOpen" and click on it
+    // document.getElementById("defaultOpen").click();
 
 
 
